4e46b3bc45a287f65b17e21bfc4c3f428c70ac00
Nandini P <nandini.palakollu@capillarytech.com>
2024-02-06 17:00:54 +0530

    testing1

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -2,14 +2,13 @@ import { AddTasksSaga } from "./redux saga/AddTasksSaga";
 import { DeleteTasksSaga } from "./redux saga/DeleteTasksSaga";
 import { EditTasksSaga } from "./redux saga/EditTasksSaga";
 import { fetchTasksSaga } from "./redux saga/FetchTasksSaga";
-import {all} from 'redux-saga/effects';
-export function* rootSaga(){
-    yield all(
-        [
-            fetchTasksSaga(),
-            AddTasksSaga(),
-            DeleteTasksSaga(),
-            EditTasksSaga(),
-        ]
-    )
+import { all } from "redux-saga/effects";
+export function* rootSaga() {
+  yield all([
+    fetchTasksSaga(),
+    AddTasksSaga(),
+    DeleteTasksSaga(),
+    EditTasksSaga(),
+  ]);
 }
+//testing1
